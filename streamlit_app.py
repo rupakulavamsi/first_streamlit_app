c88969f2942b059cdc47a2d71270a14a2fd8e214
rupakulavamsi <142319345+rupakulavamsi@users.noreply.github.com>
2023-08-15 23:23:52 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit
 
-streamlit.title('my new exercize routine')
-streamlit.header('Cardio n Weights')
-streamlit.text('Squats')
-streamlit.text('Plank')
+streamlit.title('my parents new healthy diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
